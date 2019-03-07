aa39aa52cccaee838c1419e978902920ad2d9e40
Elizaveta Starikova <hed.robin@gmail.com>
2019-03-07 18:55:54 +0300

    Add files via upload
    added phage.sh

---
+++ phage.sh
@@ -0,0 +1,85 @@
+z=0.1
+clear
+printf "\n \n\ \n|\n/\n\n\n\n\n/\ \n  \ \n"
+sleep $z
+clear
+printf " \n\ \n_\ \n/|\n /\n/\n'\n \n \n /\ \n^  \ \n"
+sleep $z
+clear
+printf "\ \n \ \n__\ \n /|\n/ /\n\/\n|'\n| \n| \n| /\ \n^^  \ \n"
+sleep $z
+clear
+printf "/\ \n  \ \n___\ \n  /|\n\/ /\n/\/\n||'\n|| \n|| \n|| /\ \n^^^  \ \n"
+sleep $z
+clear
+printf " /\ \n/  \ \n____\ \n\  /|\n \/ /\n\/\/\n'||'\n || \n || \n || /\ \n^^^^  \ \n"
+sleep $z
+clear
+printf "  /\ \n /  \ \n/____\ \n|\  /|\n\ \/ /\n \/\/\n '||'\n  || \n  || \n\ || /\ \n ^^^^  \ \n"
+sleep $z
+clear
+printf "   /\ \n  /  \ \n /____\ \n |\  /|\n \ \/ /\n  \/\/\n  '||'\n   || \n   || \n/\ || /\ \n  ^^^^  \ \n"
+sleep $z
+clear
+printf "    /\ \n   /  \ \n  /____\ \n  |\  /|\n  \ \/ /\n   \/\/\n   '||'\n    || \n    || \n /\ || /\ \n/  ^^^^  \ \n"
+sleep $z
+clear
+printf "     /\ \n    /  \ \n   /____\ \n   |\  /|\n   \ \/ /\n    \/\/\n    '||'\n     || \n     || \n  /\ || /\ \n /  ^^^^  \ \n"
+sleep $z
+clear
+printf "    /\ \n   /  \ \n  /____\ \n  |\  /|\n  \ \/ /\n   \/\/\n   '||'\n    || \n    || \n /\ || /\ \n/  ^^^^  \ \n"
+sleep $z
+clear
+printf "     /\ \n    /  \ \n   /____\ \n   |\  /|\n   \ \/ /\n    \/\/\n    '||'\n     || \n     || \n  /\ || /\ \n /  ^^^^  \ \n"
+sleep $z
+clear
+printf "     /\ \n    /  \ \n   /____\ \n   |\  /|\n   \ \/ /\n    \/\/\n    '||'\n     || \n     ||  ____\n  /\ || /  \n /  ^^^^    \n"
+sleep $z
+clear
+printf "      /\ \n     /  \ \n    /____\ \n    |\  /|\n    \ \/ /\n     \/\/\n     '||'\n      || \n      || \n   /\ || /\ \n  /  ^^^^  \ \n"
+sleep $z
+clear
+printf "     /\ \n    /  \ \n   /____\ \n   |\  /|\n   \ \/ /\n    \/\/\n    '||'\n     || \n     ||  ____\n  /\ || /  \n /  ^^^^    \n"
+sleep $z
+clear
+printf "     /\ \n    /  \ \n   /____\ \n   |\  /|\n   \ \/ /\n    \/\/\n    '||'\n     || \n     || \n  /\ || /\ \n /  ^^^^  \ \n"
+sleep $z
+clear
+printf "     /\ \n    /  \ \n   /____\ \n   |\  /|\n   \ \/ /\n    \/\/\n    '||'\n     || \n___  || \n   \ || /\ \n    ^^^^  \ \n"
+sleep $z
+clear
+printf "     /\ \n    /  \ \n   /____\ \n   |\  /|\n   \ \/ /\n    \/\/\n    '||'\n     || \n     ||  ____\n  /\ || /  \n /  ^^^^    \n"
+sleep $z
+clear
+printf "      /\ \n     /  \ \n    /____\ \n    |\  /|\n    \ \/ /\n     \/\/\n     '||'\n      || \n      || \n   /\ || /\ \n  /  ^^^^  \ \n"
+sleep $z
+clear
+printf "     /\ \n    /  \ \n   /____\ \n   |\  /|\n   \ \/ /\n    \/\/\n    '||'\n     || \n     || \n  /\ || /\ \n /  ^^^^  \ \n"
+sleep $z
+clear
+printf "    /\ \n   /  \ \n  /____\ \n  |\  /|  -SAIRGA!\n  \ \/ /\n   \/\/\n   '||'\n    || \n    || \n /\ || /\ \n/  ^^^^  \ \n"
+sleep $z
+clear
+printf "   /\ \n  /  \ \n /____\ \n |\  /|  -SAIRGA!\n \ \/ /\n  \/\/\n  '||'\n   || \n   || \n/\ || /\ \n  ^^^^  \ \n"
+sleep $z
+clear
+printf "  /\ \n /  \ \n/____\ \n|\  /|\n\ \/ /\n \/\/\n '||'\n  || \n  || \n\ || /\ \n ^^^^  \ \n"
+sleep $z
+clear
+printf " /\ \n/  \ \n____\ \n\  /|\n \/ /\n\/\/\n'||'\n || \n || \n || /\ \n^^^^  \ \n"
+sleep $z
+clear
+printf "/\ \n  \ \n___\ \n  /|\n\/ /\n/\/\n||'\n|| \n|| \n|| /\ \n^^^  \ \n"
+sleep $z
+clear
+printf "\ \n \ \n__\ \n /|\n/ /\n\/\n|'\n| \n| \n| /\ \n^^  \ \n"
+sleep $z
+clear
+printf " \n\ \n_\ \n/|\n /\n/\n'\n \n \n /\ \n^  \ \n"
+sleep $z
+clear
+printf "\n \n\ \n|\n/\n\n\n\n\n/\ \n  \ \n"
+sleep $z
+clear
+
+
